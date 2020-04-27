31add646563da5b55b33eb4add57c5a8784404e8
John Park <e2npark@gmail.com>
2020-04-27 22:31:06 +0900

    update profileserver initially

---
+++ internal/server/profileservice.go
@@ -0,0 +1,127 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+	"profileserver-golang-kuberntes/internal/config"
+	"profileserver-golang-kuberntes/internal/data"
+	"profileserver-golang-kuberntes/internal/dynamo"
+	"profileserver-golang-kuberntes/internal/log"
+	"profileserver-golang-kuberntes/internal/logger"
+	"profileserver-golang-kuberntes/internal/rediscache"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+// ProfileService is a list of service
+type ProfileService struct {
+	log *logger.Logger
+}
+
+// Init is to start Profile Service
+func (p *ProfileService) Init(conf *config.AppConfig) error {
+	p.log = logger.NewLogger("ProfileServer")
+
+	// initialize radis for in-memory cache
+	rediscache.NewRedisCache(conf.Redis)
+
+	// Initiate the dynamo database
+	error := dynamo.NewDatabase(conf.Dynamo)
+	if error != nil {
+		log.D("Faile to open dynamodb: %v", error.Error())
+		return error
+	}
+
+	return nil
+}
+
+// Start is to run Profile Server
+func (p *ProfileService) Start() error {
+	log.I("start Profile Server...")
+
+	// Init Router
+	r := mux.NewRouter()
+
+	// Route Handler / Endpoints
+	r.HandleFunc("/add", Insert).Methods("POST")
+	r.HandleFunc("/search/{key}", Retrieve).Methods("GET")
+	r.HandleFunc("/", LiveCheck).Methods("GET")
+
+	var err error
+	err = http.ListenAndServe(":8080", r)
+
+	return err
+}
+
+// OnTerminate is to close the servcie
+func (p *ProfileService) OnTerminate() error {
+	log.I("Profile Server was terminated")
+
+	// To-Do: add codes for error cases if requires
+	return nil
+}
+
+// Insert is the api to append an Item
+func Insert(w http.ResponseWriter, r *http.Request) {
+	// parse the data
+	var item data.UserProfile
+	_ = json.NewDecoder(r.Body).Decode(&item)
+	log.D("item: %+v", item)
+
+	if err := dynamo.Write(item); err != nil {
+		log.E("Got error calling PutItem: %v", err.Error())
+
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// put the item into rediscache
+	key := item.UID // UID to identify the profile
+	_, rErr := rediscache.SetCache(key, &item)
+	if rErr != nil {
+		log.E("Error of setCache: %v", rErr)
+		w.WriteHeader(http.StatusServiceUnavailable)
+		return
+	}
+	log.D("Successfully inserted in redis cache")
+
+	w.WriteHeader(http.StatusOK)
+}
+
+// Retrieve is the api to search an Item
+func Retrieve(w http.ResponseWriter, r *http.Request) {
+	uid := strings.Split(r.URL.Path, "/")[2]
+	log.D("Looking for uid: %v ...", uid)
+
+	// search in redis cache
+	cache, err := rediscache.GetCache(uid)
+	if err != nil {
+		log.E("Error: %v", err)
+	}
+	if cache != nil {
+		log.D("value from cache: %+v", cache)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(cache)
+	} else {
+		log.D("No data in redis cache then search it in database.")
+
+		// search in database
+		item, err := dynamo.Read(uid)
+		if err != nil {
+			log.D("Fail to read: %v", err.Error())
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		log.D("%v", item)
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(item)
+	}
+}
+
+// LiveCheck is the api to check the pod is alive
+func LiveCheck(w http.ResponseWriter, r *http.Request) {
+	log.D("Live Check ...")
+}
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"profileserver-golang-kuberntes/internal/config"
+	"profileserver-golang-kuberntes/internal/log"
+	"profileserver-golang-kuberntes/internal/server"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var (
+	serviceList []*server.BaseService
+	conf        *config.AppConfig
+	wg          sync.WaitGroup
+)
+
+//ExitSuccess is exit code 0 and ExitFailure is exit code 1
+const (
+	ExitSuccess = iota
+	ExitFailure
+)
+
+func main() {
+	log.I("Starting service ...")
+	err := Initialize()
+	if err != nil {
+		log.E("Failed to initialize service: %v", err)
+		os.Exit(1)
+	}
+
+	err = StartService()
+	if err != nil {
+		log.E("Failed to start service: %v", err)
+		os.Exit(1)
+	}
+	log.E("Exiting service ...")
+}
+
+// Initialize is to setup the basic
+func Initialize() error {
+	log.I("initiate the service...")
+
+	// Configuration loading
+	var configFileName string = "configs/config.json"
+
+	conf = config.GetInstance()
+	if !config.Load(configFileName) {
+		err := fmt.Errorf("Failed to load config file: %s", configFileName)
+		return err
+	}
+	log.D("Configuration has been loaded.")
+
+	// Setup log level
+	log.SetupLogger(conf.Logging.Enable, conf.Logging.Level)
+
+	// Setup signal handlers for interruption and termination
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		for sig := range sigCh {
+			if sig == syscall.SIGINT || sig == syscall.SIGTERM {
+				log.D("Graceful Termination Time = %d", conf.GracefulTermTimeMillis)
+				Finalize()
+				time.Sleep(time.Duration(conf.GracefulTermTimeMillis) * time.Millisecond)
+				os.Exit(ExitFailure)
+			}
+		}
+	}()
+
+	return nil
+}
+
+// StartService starts all the component of this service.
+func StartService() error {
+	log.I("start the service...")
+
+	conf = config.GetInstance()
+
+	// if there are more services, those can be appened here
+	serviceList = append(serviceList, server.NewBaseService(&server.ProfileService{}, &wg, conf))
+
+	for _, service := range serviceList {
+		go service.Run()
+	}
+
+	wg.Wait()
+
+	return nil
+}
+
+// Finalize cleans up this service including wrapping up current
+// DB transaction and closing open DB connection before shutting
+// down this service
+func Finalize() {
+	//	db.Close()
+	rediscache.Close()
+	for _, service := range serviceList {
+		service.Stop()
+	}
+	log.E("Shutdown service...")
+}
