844df8621ab0d79bdcb95f2c4ac16adf51505849
John Park <52392004+kyopark2014@users.noreply.github.com>
2020-06-04 09:29:12 +0900

    Update rediscache.go

--- internal/rediscache/rediscache.go
+++ internal/rediscache/rediscache.go
@@ -1,23 +1,21 @@
 package rediscache
 
 import (
-	"encoding/json"
 	"profileserver-golang-kuberntes/internal/config"
-	"profileserver-golang-kuberntes/internal/data"
 	"profileserver-golang-kuberntes/internal/logger"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
 )
 
-var (
-	pool *redis.Pool
-	ttl  int
-	log  *logger.Logger
-)
+var pool *redis.Pool
+
+var ttl int
+
+var log *logger.Logger
 
 func init() {
-	log = logger.NewLogger("rediscache")
+	log = logger.NewLogger("webchat")
 }
 
 // NewRedisCache is to set the configuration for redis
@@ -25,7 +23,7 @@ func NewRedisCache(cfg config.RedisConfig) {
 	pool = newPool(cfg)
 	ttl = cfg.TTL
 
-	log.I("Successfully connected to redis cache: %v:%v (ttl: %v)", cfg.Host, cfg.Port, cfg.TTL)
+	log.I("Redis: %v:%v (ttl: %v)", cfg.Host, cfg.Port, cfg.TTL)
 }
 
 // Close to disconnect the connection of redis
@@ -58,34 +56,22 @@ func newPool(cfg config.RedisConfig) *redis.Pool {
 }
 
 // GetCache is to get the data from redis
-func GetCache(key string) (*data.UserProfile, error) {
+func GetCache(key string) (string, error) {
 	c := pool.Get()
 	defer c.Close()
 
 	raw, err := redis.String(c.Do("GET", key))
 	if err == redis.ErrNil {
-		return nil, nil
+		return raw, nil
 	} else if err != nil {
-		return nil, err
+		return raw, err
 	}
 
-	var value *data.UserProfile
-	err = json.Unmarshal([]byte(raw), &value)
-	if err != nil {
-		log.E("%v: %v", key, err)
-		return nil, err
-	}
-	return value, err
+	return raw, err
 }
 
 // SetCache is to record the data in redis
-func SetCache(key string, value *data.UserProfile) (interface{}, error) {
-	raw, err := json.Marshal(*value)
-	if err != nil {
-		log.E("%v: %v", key, err)
-		return nil, err
-	}
-
+func SetCache(key string, raw []byte) (interface{}, error) {
 	c := pool.Get()
 	defer c.Close()
 
@@ -96,6 +82,77 @@ func SetCache(key string, value *data.UserProfile) (interface{}, error) {
 	} else {
 		return c.Do("SETEX", key, ttl, raw)
 	}
+}
+
+// PushList is to record the data in redis
+func PushList(key string, raw []byte) (interface{}, error) {
+	c := pool.Get()
+	defer c.Close()
+
+	log.D("LPUSH: key: %s, value: %v", key, string(raw))
+
+	return c.Do("LPUSH", key, raw)
+}
+
+// GetList is to record the data in redis
+func GetList(key string) ([]string, error) {
+	c := pool.Get()
+	defer c.Close()
+
+	raw, err := redis.Strings(c.Do("LRANGE", key, 0, -1))
+	log.D("raw: %v", raw)
+	if err == redis.ErrNil {
+		return raw, nil
+	} else if err != nil {
+		return raw, err
+	}
+
+	return raw, err
+}
+
+// Publish is to send data in redis PUBSUB
+func Publish(key string, raw []byte) (interface{}, error) {
+	c := pool.Get()
+	defer c.Close()
+
+	log.D("PUBLISH: %s %v", key, string(raw))
+
+	return c.Do("PUBLISH", key, raw)
+}
+
+// Subscribe is to get message event in redis
+func Subscribe(channel string, d chan []byte) error {
+	c := pool.Get()
+	defer c.Close()
+
+	go func() {
+		// Get a connection from a pool
+		c := pool.Get()
+		psc := redis.PubSubConn{Conn: c}
+
+		// Set up subscriptions
+		if err := psc.Subscribe(channel); err != nil {
+			log.E("Subscribe error: %v", err.Error)
+		}
+
+		// While not a permanent error on the connection.
+		for c.Err() == nil {
+			switch v := psc.Receive().(type) {
+			case redis.Message:
+				d <- v.Data
+				log.D("message: %s %s\n", v.Channel, v.Data)
+			case redis.Subscription:
+				log.E("subscribed: %s %s %d\n", v.Channel, v.Kind, v.Count)
+			case error:
+				log.E("%v", error.Error)
+				return
+			}
+		}
+
+		psc.Unsubscribe()
+		c.Close()
+	}()
+	return nil
 }
 
 // Del deletes key.
